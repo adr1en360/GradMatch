275f814aa550c3c1d095eca019e1510d39b070ee
Adam <adamlawal669@gmail.com>
2025-05-17 09:54:43 +0100

    set up the first backend models

--- backend/project/admin.py
+++ backend/project/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date','program', 'gpa', 'gre')
+    list_filter = ('user', 'date')
+    search_fields = ('user__username', 'program')
+    ordering = ('-date',) # Enable autocomplete for the user field
+
+    
+    
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'ranking', 'acceptance_rate', 'avg_gre', 'avg_gpa')
+    list_filter = ('location', 'ranking')
+    search_fields = ('name', 'location')
+    ordering = ('ranking',)
+    
+admin.site.register(UserData, UserDataAdmin)
+admin.site.register(University, UniversityAdmin)
--- backend/project/models.py
+++ backend/project/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class UserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    date = models.DateTimeField(auto_now_add=True)
+    program = models.CharField(null = True, blank = True, max_length=100)   
+    gpa = models.FloatField(null = True, blank = True)
+    gre = models.FloatField(null = True, blank = True)
+    def __str__(self):
+        return f"Tokens for {self.user.username}"
+    
+class University(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    ranking = models.IntegerField()
+    acceptance_rate = models.FloatField()
+    avg_gre = models.FloatField()
+    avg_gpa = models.FloatField()    
+    def __str__(self):
+        return f'{self.name} - {self.location} -  {self.ranking} - {self.acceptance_rate} -  {self.avg_gre} - {self.avg_gpa}'
