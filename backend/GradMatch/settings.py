cacdbf1bcaf72788b18c8c5c73cf7188a6fd39b7
Your Name <you@example.com>
2025-05-19 00:29:41 +0100

    one more

--- backend/GradMatch/settings.py
+++ backend/GradMatch/settings.py
@@ -37,12 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'debug_toolbar',
     'project'
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,8 +49,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
-INTERNAL_IPS = ['127.0.0.1']
 
 ROOT_URLCONF = "GradMatch.urls"
 
