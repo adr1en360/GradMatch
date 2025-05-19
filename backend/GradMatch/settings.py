c74a1e3733231a21cbb8d876a51e0eda17fc79d5
Adam <adamlawal669@gmail.com>
2025-05-19 08:17:54 +0100

    update

--- backend/GradMatch/settings.py
+++ backend/GradMatch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-mh(hj*3-(g1)+t-g-)hlfckiee!^h^kh8&8v2%-0l$e57!!f%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["gradmatch.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["gradmatch.onrender.com", "localhost", '127.0.0.1']
 
 
 # Application definition
