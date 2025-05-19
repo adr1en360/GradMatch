aae01b654945a4500db08319dfdb13b3ac21b8d1
Your Name <you@example.com>
2025-05-19 01:29:25 +0100

    .

--- backend/GradMatch/settings.py
+++ backend/GradMatch/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,7 @@ ROOT_URLCONF = "GradMatch.urls"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'project' / 'templates'],
+        'DIRS': [BASE_DIR, 'project', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             "context_processors": [
@@ -118,7 +119,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
